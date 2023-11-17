ceb5a24b04452a87633b8d85b9884f5a20bd502f
Fisselo <yvandouis@hotmail.fr>
2023-11-17 12:08:45 +0100

    Code améliorer deplacement + new sprite

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,34 +1,25 @@
-extends Node2D
+extends CharacterBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 var speed = 500
-const limitxright = 970
-const limitxleft = -120
-const limitydown = -15
-const limityup = 565
-
+var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	screen_size = get_viewport_rect().size
+	velocity.x = 0
+	velocity.y = 0
 	
-func _input(event):
-	print(event.as_text())
-	if (event.is_action_pressed("ui_right")):
-		print("aj")
-	
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_pressed("ui_right") && !(position.x > limitxright)):
-		position.x += speed * delta
-	if Input.is_action_pressed("ui_left")  && !(position.x < limitxleft):
-		position.x -= speed * delta
-	if Input.is_action_pressed("ui_up") && !(position.y < limitydown):
-		position.y -= speed * delta
-	if Input.is_action_pressed("ui_down")&& !(position.y > limityup):
-		position.y += speed * delta
+	get_input()
+	move_and_slide()
+	
 	
 
 func collision(area):
