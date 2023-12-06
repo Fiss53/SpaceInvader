d7354afbd325603c29e5856180b50871bc091d58
Yvan <yvandouis@hotmail.fr>
2023-12-06 08:05:25 +0100

    Meteorite brille

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -11,7 +11,7 @@ var vitesse = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_node("Sprite2D").play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
